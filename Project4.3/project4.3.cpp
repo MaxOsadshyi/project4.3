cc2395ab65372f6adc9622240d56935731c0bf8e
MaxOsadshyi <maksym.osadchyi.ik.2023@lpnu.ua>
2023-10-10 15:02:46 +0300

    max commit4.3

---
+++ Project4.3/project4.3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx,a,b,c,F;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "------------" << endl;
+	cout <<"|" << setw(10) << "F" << "|" << endl;
+	cout << "------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <0&&b!=0)
+			F = a*x*x-b*x*x;
+		else
+			if (x>0&&b!=0)
+				F = (x-a)/(x-c);
+			else
+				F = (x+5)/(c*(x-10));
+		cout 
+			<< "|" << setw(9) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "------------" << endl;
+	return 0;
+}
